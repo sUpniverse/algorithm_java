e1eb9e521c90f53e187b90f7d83d7f0eabb0d479
supniverse <kmsup2@gmail.com>
2021-03-26 23:25:05 +0900

    not solved

--- sort/1449_repairmanHangseung/src/main/java/Main.java
+++ sort/1449_repairmanHangseung/src/main/java/Main.java
@@ -19,8 +19,14 @@ public class Main {
 
     int count = 0;
     int now = spot[0];
+    count++;
     for(int i = 0; i < spotSize; i++) {
-
+      if(spot[i] - now > length - 1) {
+        count++;
+        now = spot[i];
+      } else {
+        now = spot[i+1];
+      }
     }
 
     System.out.println(count);
