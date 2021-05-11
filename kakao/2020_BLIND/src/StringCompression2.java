cfb310f4311e2dfdd84e7963c39fa69c165f8d86
supniverse <kmsup2@gmail.com>
2021-05-11 22:05:46 +0900

    not solve

--- kakao/2020_BLIND/src/StringCompression2.java
+++ kakao/2020_BLIND/src/StringCompression2.java
@@ -6,24 +6,33 @@ public class StringCompression2 {
 
   public static int solution(String s) {
 
-    StringBuilder sb = new StringBuilder();
 
     int n = Integer.MAX_VALUE;
+    int answer = 0;
 
+    StringBuilder sb = new StringBuilder();
     // 문자를 잘라야 하는 숫자의 범위
     for(int i = 1; i < n; i++) {
-      // 단어 하나하나 탐색
-      for(int j = 0; j < s.length(); j+=) {
+      sb.setLength(0);
+      // 단어 하나하나 탐색 0 부터 n까지
+      for(int j = 0; j < s.length()/2; j+=i) {
         int same = 0;
-        String substring = s.substring(j, j + i);
+        // 숫자의 범위에 따른 압축개수 설정
+        String substring = s.substring(j, j+i-1);
+        // 압축할 문자열의 길이 + i 만큼부터 갯수 카운팅
         for(int k = j+i; k < s.length(); k+=i) {
-          if(!substring.equals(s.substring(k, k+i))) {
+          // 연달아 같은 문자열이 아니라면 취소
+          if(!substring.equals(s.substring(k, k+i-1))) {
             break;
           }
           same++;
         }
+        if(same > 0) {
+          sb.append(same).append(substring);
+        }
       }
+      answer = Math.min(answer, sb.length());
     }
+    return answer;
   }
-
 }
--- kakao/2020_BLIND/test/StringCompressionTest.java
+++ kakao/2020_BLIND/test/StringCompressionTest.java
@@ -1,20 +1,21 @@
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
+
 
 class StringCompressionTest {
 
     @Test
     public void compressionTest() {
 
-        assertEquals(1, StringCompression.solution("a"));
-        assertEquals(2, StringCompression.solution("aaaaa"));
-        assertEquals(3, StringCompression.solution("aaaaaaaaaa"));
-        assertEquals(7, StringCompression.solution("aabbaccc"));
-        assertEquals(9, StringCompression.solution("ababcdcdababcdcd"));
-        assertEquals(8, StringCompression.solution("abcabcdede"));
-        assertEquals(14, StringCompression.solution("abcabcabcabcdededededede"));
-        assertEquals(17, StringCompression.solution("xababcdcdababcdcd"));
+        assertEquals(1, StringCompression2.solution("a"));
+        assertEquals(2, StringCompression2.solution("aaaaa"));
+        assertEquals(3, StringCompression2.solution("aaaaaaaaaa"));
+        assertEquals(7, StringCompression2.solution("aabbaccc"));
+        assertEquals(9, StringCompression2.solution("ababcdcdababcdcd"));
+        assertEquals(8, StringCompression2.solution("abcabcdede"));
+        assertEquals(14, StringCompression2.solution("abcabcabcabcdededededede"));
+        assertEquals(17, StringCompression2.solution("xababcdcdababcdcd"));
     }
 
 }
